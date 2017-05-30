a15f770abac1543f0b3920bcc16032d26d3d8d48
nagura4000 <nagura4000@gmail.com>
2017-05-30 21:38:42 +0900

    気温追加

--- cuc/main.py
+++ cuc/main.py
@@ -10,6 +10,9 @@ from os.path import os
 from bottle import static_file
 import mysql.connector
 from bottle import redirect
+import time
+import urllib.request
+import json
 # install(PgSQLPlugin('dbname=sawachi, user=cuc password='))
 
 # @route('/hello')
@@ -22,9 +25,9 @@ from bottle import redirect
 # def test():
 #   import mysql.connector
 #   connect = mysql.connector.connect(db='sawachi', host='localhost', port=3306, user='root', passwd='')
-# 
+#
 #   cur=connect.cursor()
-#   cur.execute("SELECT * FROM client_mst")                                                                                      
+#   cur.execute("SELECT * FROM client_mst")
 #   result = cur.fetchall()
 #   name = result[0][1]
 #   return template('hello', name=name)
@@ -58,40 +61,40 @@ def font_dir(filename):
 @route('/employee')
 def employee():
   cur=connect.cursor()
-  cur.execute("SELECT * FROM employee_mst")                                                                                      
+  cur.execute("SELECT * FROM employee_mst")
   result = cur.fetchall()
   return template('employee', rows=result)
-  
+
 @route('/planning')
 def planning():
   cur=connect.cursor()
-  cur.execute("SELECT * FROM employee_mst")                                                                                      
+  cur.execute("SELECT * FROM employee_mst")
   employee = cur.fetchall()
   return template('production_planning', rows=employee)
-  
+
 @route('/production')
 def production():
   cur=connect.cursor()
-  sql = """     SELECT 
+  sql = """     SELECT
                     pp.id,
-                    em.name, 
+                    em.name,
                     mm.name,
                     im.name,
                     pp.production_number,
                     pp.production_date ,
                     ifnull(pi.success_number, 0)
-                FROM production_planning as pp 
+                FROM production_planning as pp
                 join employee_mst em on (pp.id_employee_mst = em.id)
                 join machine_mst mm on (pp.id_machine_mst = mm.id)
                 join item_mst as im on (pp.id_item_mst = im.id)
-                left join ( select     
+                left join ( select
                                     pid.id_production_planning,
                                     sum(pid.production_number) as success_number
-                            from production_info as pid 
+                            from production_info as pid
                             group by pid.id_production_planning
                             ) as pi on  (pp.id = pi.id_production_planning)
         """
-  cur.execute(sql)                                                                                      
+  cur.execute(sql)
   production_planning = cur.fetchall()
   return template('production', rows=production_planning)
 
@@ -99,42 +102,42 @@ def production():
 def production_detail():
     pralningid = request.query.get('planningId')
     cur=connect.cursor()
-    sql = """     SELECT 
+    sql = """     SELECT
                       pp.id,
-                      em.name, 
+                      em.name,
                       mm.name,
                       im.name,
                       pp.production_number,
-                      pp.production_date 
-                  FROM production_planning as pp 
+                      pp.production_date
+                  FROM production_planning as pp
                   join employee_mst em on (pp.id_employee_mst = em.id)
                   join machine_mst mm on (pp.id_machine_mst = mm.id)
                   join item_mst as im on (pp.id_item_mst = im.id)
                   where pp.id = {pralningid}
         """.format(**vars());
-    cur.execute(sql)                                                                                      
+    cur.execute(sql)
     production_planning = cur.fetchall()
-    
+
     # 生産実績
-    sql = """     SELECT 
+    sql = """     SELECT
                       pi.id,
-                      pi.production_number, 
+                      pi.production_number,
                       pi.failure_number,
                       pi.create_start_time,
                       pi.create_end_time
-                      
+
                       , wm.name
-                      
-                  FROM production_info as pi 
-                  
+
+                  FROM production_info as pi
+
                   left join stock_info as si on (si.id_production_info = pi.id)
                   left join warehouse_mst wm on (wm.id = si.id_warehouse)
-                  
+
                   where pi.id_production_planning = {pralningid}
         """.format(**vars());
     cur.execute(sql)
     production_infos = cur.fetchall()
-    
+
     return template('production_detail', production_planning=production_planning[0], production_infos=production_infos)
 
 @route('/production_detail', method='POST')
@@ -145,12 +148,12 @@ def registere_detail():
     start_date = request.forms.get('start_date')
     end_date = request.forms.get('end_date')
     cur=connect.cursor()
-    sql = """     insert into 
+    sql = """     insert into
                     production_info
                     (
                         id_production_planning,
-                        production_number, 
-                        failure_number, 
+                        production_number,
+                        failure_number,
                         create_start_time,
                         create_end_time
                     )
@@ -173,10 +176,10 @@ def stock_view():
     id_production_info = request.query.get('infoId')
     cur=connect.cursor()
     # 生産実績
-    sql = """     SELECT 
+    sql = """     SELECT
                       pi.id,
                       pi.id_production_planning,
-                      pi.production_number, 
+                      pi.production_number,
                       em.name,
                       im.name,
                       si.id,
@@ -186,7 +189,7 @@ def stock_view():
                       si.depth,
                       si.instock_time,
                       si.outstock_time
-                  FROM production_info as pi 
+                  FROM production_info as pi
                   JOIN production_planning as pp on (pi.id_production_planning = pp.id)
                   join employee_mst em on (pp.id_employee_mst = em.id)
                   join machine_mst mm on (pp.id_machine_mst = mm.id)
@@ -196,12 +199,12 @@ def stock_view():
                   where pi.id = {id_production_info}
         """.format(**vars());
 
-    cur.execute(sql)                                                                                      
+    cur.execute(sql)
     production_infos = cur.fetchall()
-    
+
     cur.execute("SELECT * FROM warehouse_mst")
     warehouses = cur.fetchall()
-    
+
     return template('stock', production_infos=production_infos, warehouses=warehouses)
 
 @route('/stock', method='POST')
@@ -213,12 +216,12 @@ def registere_stock():
     depth = request.forms.get('depth')
     instock_time = request.forms.get('instock_time')
     cur=connect.cursor()
-    sql = """     insert into 
+    sql = """     insert into
                     stock_info
                     (
                         id_production_info,
-                        id_warehouse, 
-                        row, 
+                        id_warehouse,
+                        row,
                         col,
                         depth,
                         instock_time
@@ -249,6 +252,54 @@ def stock_detail():
 def video():
     print("aaa")
     return template('video')
+
+@route('/temperature')
+def temperature():
+  cur=connect.cursor()
+  #cur.execute("SELECT * FROM temperature order by id desc;")
+  cur.execute("SELECT * FROM temperature order by id desc limit 40;")
+  #cur.execute("SELECT * FROM temperature order by id desc limit 20;")
+
+  result = cur.fetchall()
+  return template('temperature', rows=result)
+
+@route('/put_temperature', method='GET')
+def registere_temperature():
+
+    #location = "Tokyo,jp"
+    lat = "35.741940"
+    lon = "139.906838"
+    mode = "json"
+    metric = "metric"
+    apikey = "a7a0c4494d0691013138ed27002a01b4"
+    url = "http://api.openweathermap.org/data/2.5/weather?lat={a}&lon={aa}&mode={b}&units={c}&APPID={d}".format(a=lat, aa=lon, b=mode, c=metric, d=apikey)
+    response = urllib.request.urlopen(url)
+    weather_json = json.loads(response.read().decode('utf8'))
+    out_temperature = weather_json['main']['temp']
+
+    temperature = request.query.get('temperature')
+    humidity = request.query.get('humidity')
+    update_time = time.strftime('%Y-%m-%d %H:%M:00')
+    cur=connect.cursor()
+    sql = """     insert into
+                    temperature
+                    (
+                        temperature,
+                        humidity,
+                        update_time
+                    )
+                    values (
+                        {temperature},
+                        {out_temperature},
+                        '{update_time}'
+                    )
+    """.format(**vars());
+    print(sql)
+    cur.execute(sql)
+    connect.commit()
+    redirect('/temperature')
+
+
 
 #run (host='localhost', port=8080, debug=True)
 run(host='0.0.0.0', port=80)
---
+++ cuc/views/temperature.tpl
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>気温</title>
+	    <!-- BootstrapのCSS読み込み -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	    <!-- jQuery読み込み -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <!-- BootstrapのJS読み込み -->
+	    <script src="js/bootstrap.min.js"></script>
+	    <script src="js/canvasjs.min.js"></script>
+    </head>
+    <body>
+
+    	<div id="stage"></div>
+		<script>
+var data = [
+% for row in reversed(rows):
+	{ label: "{{row[3]}}", y: {{row[1]}} },
+% end
+];
+
+var data2 = [
+% for row in reversed(rows):
+	{ label: "{{row[3]}}", y: {{row[2]}} },
+% end
+];
+var stage = document.getElementById('stage');
+var chart = new CanvasJS.Chart(stage, {
+  title: {
+    text: "気温"  //グラフタイトル
+  },
+  theme: "theme4",  //テーマ設定
+  data: [{
+    type: 'line',		//グラフの種類
+    name: "センサー気温",
+    legendText: "センサー気温",
+    showInLegend: true,
+    dataPoints: data	//表示するデータ
+  },{
+    type: 'line',		//グラフの種類
+    name: "外気温",
+    legendText: "外気温",
+    showInLegend: true,
+    dataPoints: data2	//表示するデータ
+  }]
+});
+chart.render();
+		</script>
+<!--
+    	<div class="container">
+    		<table class="table table-striped table-bordered table-hover ">
+    		    <thead>
+			      <tr>
+			        <th>日時</th>
+			        <th>気温</th>
+			        <th>湿度</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			    	% for row in rows:
+					<tr>
+						<td>{{row[3]}}</td>
+						<td>{{row[1]}}</td>
+						<td>{{row[2]}}</td>
+					</tr>
+		        	% end
+		        </tbody>
+			</table>
+		</div>
+-->
+    </body>
+</html>
